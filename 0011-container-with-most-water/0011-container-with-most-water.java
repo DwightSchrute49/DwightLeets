564ef333e44f97c132783e6b35f7816d3aff93ce
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2024-09-05 18:28:35 +0530

    Time: 5 ms (69.49%), Space: 57.5 MB (73.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -19,11 +19,5 @@ public class Solution {  // Changed from ContainerWithMostWater to Solution
         
         return maxArea;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] height = {1,8,6,2,5,4,8,3,7};
-        System.out.println("Maximum area: " + solution.maxArea(height)); // Output should be 49
-    }
 }
 
