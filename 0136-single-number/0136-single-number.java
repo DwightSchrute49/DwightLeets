8f98ec6391c78005e71e4c22a0f2b811f41fa1a7
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2024-09-06 21:59:28 +0530

    Time: 1 ms (99.82%), Space: 45.8 MB (78.92%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -9,12 +9,3 @@ class Solution {
         return result;
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {4, 1, 2, 1, 2};
-        Solution solution = new Solution();
-        int ret = solution.singleNumber(nums);
-        System.out.println(ret);  // Output: 4
-    }
-}
