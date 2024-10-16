3771d09cfa19aedef6ba76a1ee258373dcc873ff
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2024-10-16 12:21:22 +0530

    Time: 0 ms (100%), Space: 40.8 MB (64.3%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -1,12 +1,3 @@
-/*
-The idea behid this problem
-1. Here we start by taking the size as the sum of a, b, c.
-2. Then we use 3 variables A, B, C to count the occurance of a, b, c.
-3. Now we iterate until the size, and 
-    -> Checks the largest number among a, b, c and whether the count < 2 or whther the count of other letters is 2 and there is still letters that can be added, then we append the letter, decrement from the total count of that particular letter and increase the occurance of that letter and set others back to zero.
-    
-4. Finally return the string.
-*/
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
         int totalSize = a + b + c;
@@ -14,8 +5,7 @@ class Solution {
         int B = 0;
         int C = 0;
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<totalSize; i++) {
-            // checks a is largest and its count still < 2 or B and C =2 and there are still a that can be added 
+        for (int i=0; i<totalSize; i++) { 
             if ((a>=b && a>=c && A<2) || (B==2 && a>0) || (C==2 && a>0)) {
                 sb.append("a");
                 a -= 1;
@@ -23,7 +13,6 @@ class Solution {
                 B = 0;
                 C = 0;
             }
-            // check b is largest and its count still < 2 or A and C = 2 and there are still b that cam be added
             else if ((b>=a && b>=c && B<2) || (A==2 && b>0) || (C==2 && b>0)) {
                 sb.append("b");
                 b -= 1;
@@ -31,7 +20,6 @@ class Solution {
                 A = 0;
                 C = 0;
             }
-            // checks c is largest and its count still < 2 or B and A = 2 and there are still c that can be added
             else if ((c>=a && c>=b && C<2) || (A==2 && c>0) || (B==2 && c>0)) {
                 sb.append("c");
                 c -= 1;
