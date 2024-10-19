1475f374dc8ff9e647e3197b3bdcac4cd9b55553
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2024-10-19 11:13:52 +0530

    Time: 3 ms (98.79%), Space: 42.5 MB (26.8%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,16 +1,16 @@
 public class Solution {
-    public boolean isIsomorphic(String sString, String tString) {
-        char[] s = sString.toCharArray();
-        char[] t = tString.toCharArray();
-        int s_length=s.length;
-        int t_length=t.length;
+    public boolean isIsomorphic(String s, String t) {
+        char[] ss =s.toCharArray();
+        char[] tt =t.toCharArray();
+        int s_length=ss.length;
+        int t_length=tt.length;
         if(s_length != t_length) 
         return false;
         char[]sm=new char[256];
         char[]tm=new char[256];
         for(int i=0; i<s_length; i++){
-            char sc=s[i];
-            char tc=t[i];
+            char sc=ss[i];
+            char tc=tt[i];
             if(sm[sc] == 0 && tm[tc] == 0){
                 sm[sc] = tc;
                 tm[tc] = sc;
