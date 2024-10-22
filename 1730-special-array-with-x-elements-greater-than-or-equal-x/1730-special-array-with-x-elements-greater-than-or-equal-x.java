a7781786b77e4b724fb49c15691bd77135a1cb68
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2024-10-22 23:39:44 +0530

    Time: 2 ms (49.31%), Space: 41.1 MB (79.68%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,11 +1,13 @@
 class Solution {
     public int specialArray(int[] nums) {
+        int len=nums.length;
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++) {
-            int n = nums.length-i;
-            boolean cond1 =  n<=nums[i];
-            boolean cond2 = (i-1<0) || (n>nums[i-1]);
-            if (cond1 && cond2) return n;
+        for(int i=0;i<len;i++){
+            int n=len-i;
+            boolean cond1=n<=nums[i];
+            boolean cond2=(i-1<0)||(n>nums[i-1]);
+            if (cond1 && cond2)
+            return n;
         }
         return -1;
     }
