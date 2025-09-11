531614a4fda03b294f5a594c7c0a59a029c388be
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-09-11 10:37:49 +0530

    Time: 57 ms (41.16%), Space: 47.8 MB (40.51%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> vowels = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
+                vowels.add(ch);
+            }
+        }
+        Collections.sort(vowels);
+        StringBuilder result = new StringBuilder();
+        int idx = 0;
+        for (char ch : s.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
+                result.append(vowels.get(idx++)); 
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+}
