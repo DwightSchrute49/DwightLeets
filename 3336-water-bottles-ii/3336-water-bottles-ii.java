f8b48374a097893ee42ffb50e4cafe5af64e0fe2
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-10-02 18:05:51 +0530

    Time: 0 ms (100%), Space: 41.5 MB (5.16%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drink  = 0, empty = 0;
+        while (numBottles > 0){
+            drink += numBottles;
+            empty += numBottles;
+
+            numBottles = 0;
+
+            if (empty >= numExchange){
+                empty -= numExchange;
+                numBottles ++;
+                numExchange++;
+            }
+        }
+        return drink;
+    }
+}
