fb2293fad566989f69ba320bcfdeb95af7ba983e
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-11-02 10:25:53 +0530

    Time: 20 ms (83.94%), Space: 147.1 MB (5.11%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -23,7 +23,6 @@ class Solution {
             }
         }
         
-        // Count unguarded cells (cells with value 0)
         int unguardedCount = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
