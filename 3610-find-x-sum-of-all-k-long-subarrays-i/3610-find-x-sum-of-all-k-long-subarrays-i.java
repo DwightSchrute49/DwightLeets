cbc5b4de2569df5bb3027b60e1f7d9b1d8f376d6
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-11-04 11:35:38 +0530

    Time: 12 ms (55.17%), Space: 47 MB (7.59%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
         int n = nums.length;
@@ -10,7 +8,6 @@ class Solution {
             for (int j = i; j < i + k; j++) {
                 freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
             }
-            
             int[][] arr = new int[freq.size()][2];
             int idx = 0;
             for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
@@ -18,17 +15,14 @@ class Solution {
                 arr[idx][1] = e.getValue();
                 idx++;
             }
-            
             Arrays.sort(arr, (a, b) -> {
                 if (a[1] != b[1]) return b[1] - a[1];
                 return b[0] - a[0];
             });
-            
             int sum = 0;
             for (int j = 0; j < Math.min(x, arr.length); j++) {
                 sum += arr[j][0] * arr[j][1];
             }
-            
             ans[i] = sum;
         }
         return ans;
