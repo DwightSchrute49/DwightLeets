9efff8945882e4905e076894b4f8f7a74634ba18
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-11-20 09:18:54 +0530

    Time: 8 ms (42.33%), Space: 46.2 MB (8.08%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,7 +3,7 @@ class Solution {
         int len = s.length();
         boolean dp [] = new boolean [len +1];
         dp[0] = true;
-        Set<String> ans = new HashSet<>(wordDict);
+        HashSet<String> ans = new HashSet<>(wordDict);
         for(int i = 1 ; i <= len;  i++){
             for(int j = 0; j < i; j++){
                 if(dp[j] == true && ans.contains(s.substring(j,i))){
