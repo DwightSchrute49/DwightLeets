d2b140911b1008cc54af93c3e54ee566880f3867
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-12-11 08:40:39 +0530

    Time: 1 ms (74.01%), Space: 44 MB (44.56%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        int total = 1 << n;
+        for (int mask = 0; mask < total; ++mask) {
+            List<Integer> subset = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                if ((mask & (1 << i)) != 0) subset.add(nums[i]);
+            }
+            res.add(subset);
+        }
+        return res;
+    }
+}
