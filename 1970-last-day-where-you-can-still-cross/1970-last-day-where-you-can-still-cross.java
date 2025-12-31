b6ea6cde517250b0ae0beeb9db1a28393dc418cb
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2025-12-31 11:12:01 +0530

    Time: 19 ms (96.95%), Space: 79.3 MB (59.9%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -4,7 +4,6 @@ class Solution {
     int top, bottom;
 
     public int latestDayToCross(int row, int col, int[][] cells) {
-
         int n = row * col;
         top = n;
         bottom = n + 1;
@@ -13,12 +12,10 @@ class Solution {
         rank = new int[n + 2];
 
         for (int i = 0; i < n + 2; i++) parent[i] = i;
-
         int[][] grid = new int[row][col];
         int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
         for (int day = cells.length - 1; day >= 0; day--) {
-
             int r = cells[day][0] - 1;
             int c = cells[day][1] - 1;
             grid[r][c] = 1;
@@ -35,25 +32,21 @@ class Solution {
                     union(id, nr * col + nc);
                 }
             }
-
             if (find(top) == find(bottom)) {
                 return day;
             }
         }
         return 0;
     }
-
     int find(int x) {
         if (parent[x] != x)
             parent[x] = find(parent[x]);
         return parent[x];
     }
-
     void union(int x, int y) {
         int px = find(x);
         int py = find(y);
         if (px == py) return;
-
         if (rank[px] < rank[py]) parent[px] = py;
         else if (rank[px] > rank[py]) parent[py] = px;
         else {
