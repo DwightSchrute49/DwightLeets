5be8349833cf77a78a5875001c2feae731158156
S.Siddhartha <159871146+DwightSchrute49@users.noreply.github.com>
2026-01-03 09:34:05 +0530

    Time: 3 ms (93.85%), Space: 42.4 MB (35.38%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numOfWays(int n) {
+        int MOD = 1_000_000_007;
+        long A = 6; long B = 6;
+        for(int i =  2; i <= n; i++){
+            long a = (2 * A + 2* B) % MOD;
+            long b = (2 * A + 3 * B)% MOD;
+            A = a; B = b;
+        }
+        return (int)((A + B)% MOD);
+    }
+}
