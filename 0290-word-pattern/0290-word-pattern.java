7244b621b4548ced5ef882a32060ffe81c8403f2
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-03-27 21:42:29 +0530

    Time: 1 ms (82.41%), Space: 42.3 MB (98.92%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -5,10 +5,8 @@ class Solution {
         int pl = pattern.length();
 
         if (sl != pl) return false;
-
         Map<Character, String> map1 = new HashMap<>();
         Map<String, Character> map2 = new HashMap<>();
-
         for (int i = 0; i < pl; i++) {
             char ch = pattern.charAt(i);
             String word = words[i];
@@ -20,7 +18,6 @@ class Solution {
             } else {
                 map1.put(ch, word);
             }
-
             if (map2.containsKey(word)) {
                 if (map2.get(word) != ch) {
                     return false;
@@ -29,7 +26,6 @@ class Solution {
                 map2.put(word, ch);
             }
         }
-
         return true;
     }
 }
