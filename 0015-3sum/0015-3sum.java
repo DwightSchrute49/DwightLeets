5cb3ccde423b73e1326a10a8bfd97ebf72f97075
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-04-01 18:03:54 +0530

    Time: 30 ms (87.52%), Space: 59.4 MB (39.34%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,51 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int len = nums.length;
-        for(int i  = 0; i < len -2 ; i++){
-            if(i > 0 && nums[i] == nums [i-1]) continue;
-        int left =i+1; int right = len -1;
-        while(left < right){
-            int sum = nums[i] + nums[left] + nums[right];
-            if(sum == 0){
-                ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                while(left < right && nums[left] == nums[left + 1]) left ++;
-                while(left < right && nums[right] == nums[right -1]) right--;
-                left++; right--;
-            }
-            else if(sum < 0){
-                left ++;
-            }
-            else{
-            right--;
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // optimization 1: if current > 0 → no possible triplet
+            if (nums[i] > 0) break;
+
+            // optimization 2: smallest sum too big → break
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+
+            // optimization 3: largest sum too small → continue
+            if (nums[i] + nums[n - 2] + nums[n - 1] < 0) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
-        }
+
         return ans;
     }
 }
