0287d0ad9ca1dc6349fcdfbdc57684219c520393
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-04-10 22:17:57 +0530

    Time: 5 ms (15.7%), Space: 43.1 MB (87.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        // Create hashmap to store the pairs...
-        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
-        Hmap.put(')','(');
-        Hmap.put('}','{');
-        Hmap.put(']','[');
-        // Create stack data structure...
-        Stack<Character> stack = new Stack<Character>();
-        // Traverse each charater in input string...
-        for (int idx = 0; idx < s.length(); idx++){
-            // If open parentheses are present, push it to stack...
-            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
-                stack.push(s.charAt(idx));
-                continue;
-            }
-            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
-            // If not, we need to return false...
-            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
-                return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
             }
         }
-        // If the stack is empty, return true...
-        if (stack.size() == 0) {
-            return true;
-        }
-        return false;
+        return stack.isEmpty();        
     }
 }
