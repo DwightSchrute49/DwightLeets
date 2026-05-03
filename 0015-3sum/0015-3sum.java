c440d4eecca79beb76586658f1b100ccb2895815
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-05-03 22:26:52 +0530

    Time: 32 ms (78.89%), Space: 59.3 MB (32.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int len = nums.length;
-        for (int i = 0; i < len - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > 0) break;
-            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
-            if (nums[i] + nums[len - 2] + nums[len - 1] < 0) continue;
-            int left = i + 1;
-            int right = len - 1;
-            while (left < right) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1; int right = nums.length - 1;
+            while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++; right--;
-                }
-                else if (sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                    left++; right --;
+                while(left < right && nums[left] == nums[left-1]) left++;
+                while(left < right && nums[right] == nums[right +1]) right--;
+            }
+            else if(sum < 0){
+                left++;
+            }
+            else {
+                right--;
             }
         }
-        return ans;
+    }
+        return res;
     }
 }
