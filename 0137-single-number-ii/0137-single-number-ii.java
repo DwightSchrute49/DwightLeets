fd1a65aedaf0e37c28f02c37e45b769055cf65ee
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-05-27 01:58:07 +0530

    Time: 4 ms (59.06%), Space: 45 MB (92.79%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int bitIndex = 0; bitIndex < 32; bitIndex++){
+            int cnt = 0;
+            for(int i = 0; i < nums.length ; i++){
+                if((nums[i] & (1 << bitIndex)) != 0) { cnt++;
+            }
+        }
+            if(cnt % 3 == 1){
+                ans = ans | ( 1 << bitIndex);
+            }
+        }
+        return ans;
+    }
+}
