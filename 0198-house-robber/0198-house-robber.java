2fef3cf639ba436b730f4e2bb47bc8d43724c709
Siddhartha Singh <159871146+DwightSchrute49@users.noreply.github.com>
2026-07-11 15:44:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-
-        int sumEven = 0, sumOdd = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i % 2 == 0) {
-                sumEven += nums[i];
-            } else {
-                sumOdd += nums[i];
-            }
+        int n = nums.length;
+        int dp [] = new int [n];
+        if (n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0] , nums[1]);
+        dp [0] = nums [0];
+        dp [1] = Math.max (nums [1], nums[0]);
+        for(int i = 2; i < n; i ++){
+            dp [i]=  Math.max(dp[i-1] , nums[i] + dp [i-2]);
         }
-
-        int[] dp = new int[nums.length + 1];
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
-        }
-
-        return dp[nums.length];
+        return dp [n -1];
     }
 }
